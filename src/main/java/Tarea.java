e60789c432f442f10ccec81629cd67fed648ea30
Tatiana <tatisalexa16@gmail.com>
2025-04-24 08:36:03 -0500

    Actualización

--- src/main/java/Tarea.java
+++ src/main/java/Tarea.java
@@ -8,5 +8,31 @@
  * @author Personal
  */
 public class Tarea {
-    
+    private int id;
+    private String descripcion;
+    private boolean completada;
+// Constructor
+public Tarea(int id, String descripcion) {
+    this.id = id;
+    this.descripcion = descripcion;
+    this.completada = false;
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getDescripcion() {
+    return descripcion;
+}
+public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+}
+public boolean isCompletada() {
+    return completada;
+}
+public void setCompletada(boolean completada) {
+    this.completada = completada;
+}
 }
