f2d1b8e12e6ec6ecf0055e1fcb6f1f5b4e8cdc64
Tatiana <tatisalexa16@gmail.com>
2025-04-24 09:04:24 -0500

    fin

--- src/main/java/com/mycompany/integracion_completa/JakartaRestConfiguration.java
+++ src/main/java/com/mycompany/integracion_completa/JakartaRestConfiguration.java
@@ -1,7 +1,7 @@
 package com.mycompany.integracion_completa;
 
-import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
 
 /**
  * Configures Jakarta RESTful Web Services for the application.
